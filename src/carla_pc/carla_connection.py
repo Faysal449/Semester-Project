376a32eb87432938c13b8e809b706d2b9818df3b
Faysal449 <faysal-ahammed.tonmoy@stud.hshl.de>
2026-03-12 20:29:13 +0100

    Create carla_connection.py

---
+++ src/carla_pc/carla_connection.py
@@ -0,0 +1,9 @@
+So 22. Feb 23:59:56 CET 2026
+PC=192.168.0.102
+PING 192.168.0.102 (192.168.0.102) 56(84) bytes of data.
+64 bytes from 192.168.0.102: icmp_seq=1 ttl=128 time=145 ms
+64 bytes from 192.168.0.102: icmp_seq=2 ttl=128 time=5.51 ms
+
+--- 192.168.0.102 ping statistics ---
+2 packets transmitted, 2 received, 0% packet loss, time 1001ms
+rtt min/avg/max/mdev = 5.513/75.702/145.892/70.190 ms
